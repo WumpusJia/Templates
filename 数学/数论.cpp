a0f2de801b34bf7185205327ba5fc0d671d23e00
WumpusJia <qwe838866105@gmail.com>
2015-05-20 13:50:08 +0800

    new

--- 数学/数论.cpp
+++ 数学/数论.cpp
@@ -556,7 +556,24 @@ for(int i = 1;i < n;++i)
 
 
 
-
-int main()
+//数值积分
+//F为被积分函数
+double simpson(double a,double b)
 {
+    double c = a+(b-a)/2;
+    return (F(a)+4*F(c)+F(b))*(b-a)/6;
 }
+
+double asr(double a,double b,double eps,double A)
+{
+    double c = a + (b-a)/2;
+    double L = simpson(a,c), R = simpson(c,b);
+    if(fabs(L+R-A) <= 15*eps) return L+R+(L+R-A)/15.0;
+    return asr(a,c,eps/2,L) + asr(c,b,eps/2,R);
+}
+
+double asr(double a,double b,double eps)
+{
+    return asr(a,b,eps,simpson(a,b));
+}
+
