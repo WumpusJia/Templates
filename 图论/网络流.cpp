d9443ed214588274629fc5e27c6035a71e3b2743
WumpusJia <wumpusjia@gmail.com>
2015-08-16 23:07:03 +0800

    new

--- 图论/网络流.cpp
+++ 图论/网络流.cpp
@@ -248,3 +248,88 @@ int max_flow()
     }
     return flow;
 }
+
+
+//////////////////////////
+//最小费用
+
+struct Edge
+{
+    int from,to,cap,flow,cost;
+    Edge(int u,int v,int c,int f,int w):from(u),to(v),cap(c),flow(f),cost(w) {}
+};
+
+struct MCMF
+{
+    int n;
+    vector<Edge> edges;
+    vector<int> G[maxn];
+    int inq[maxn];
+    int d[maxn];
+    int p[maxn];
+    int a[maxn];
+
+    void init(int n)
+    {
+        this-> n = n;
+        for(int i = 0;i <= n;++i) G[i].clear();
+        edges.clear();
+    }
+
+    void addEdge(int from,int to,int cap,int cost)
+    {
+        edges.push_back(Edge(from,to,cap,0,cost));
+        edges.push_back(Edge(to,from,0,0,-cost));
+        int m = edges.size();
+        G[from].push_back(m-2);
+        G[to].push_back(m-1);
+    }
+
+    bool BellmanFord(int s,int t,int& flow,int& cost)
+    {
+        memset(d,INF,sizeof(d));
+        memset(inq,0,sizeof(inq));
+        d[s] = 0; inq[s] = 1; p[s] = 0; a[s] = INF;
+
+        queue<int> Q;
+        Q.push(s);
+        while(!Q.empty())
+        {
+            int u = Q.front(); Q.pop();
+            inq[u] = 0;
+            for(int i = 0;i < G[u].size();++i)
+            {
+                Edge& e = edges[G[u][i]];
+                if(e.cap > e.flow && d[e.to] > d[u] + e.cost)
+                {
+                    d[e.to] = d[u] + e.cost;
+                    p[e.to] = G[u][i];
+                    a[e.to] = min(a[u],e.cap-e.flow);
+                    if(!inq[e.to])
+                    {
+                        Q.push(e.to);
+                        inq[e.to] = 1;
+                    }
+                }
+            }
+        }
+        if(d[t] == INF) return false;
+        flow += a[t];
+        cost += d[t]*a[t];
+        int u = t;
+        while(u != s)
+        {
+            edges[p[u]].flow += a[t];
+            edges[p[u]^1].flow -= a[t];
+            u = edges[p[u]].from;
+        }
+        return true;
+    }
+
+    int getRes(int s,int t)
+    {
+        int flow = 0,cost = 0;
+        while(BellmanFord(s,t,flow,cost));
+        return cost;
+    }
+};
